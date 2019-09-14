2d3572c52bec8e582f25bfa7eb3719a3741ba3fc
Cara Coulson <47013468+caracoulson@users.noreply.github.com>
2019-09-13 17:45:27 -0700

    Basic JavaScript 3

---
+++ JS/Basic_JavaScript_3.js
@@ -0,0 +1,31 @@
+
+
+function multiplication() {
+    var simple_math = 8 * -3;
+    document.getElementById("Math").innerHTML = "8 x -3 = " + simple_math;
+}
+
+function division() {
+    var other_math = 60 / 8;
+    document.getElementById("Math_2").innerHTML = "60 / 8 = " + other_math;
+}
+
+function more_Math() {
+    var math_math = (8+6) * 13 / 8 - 35;
+    document.getElementById("math_3").innerHTML = "8 plus 6, multiplied by 13, divided by 8 and then subtracted by 35 equals " + math_math;
+}
+
+function modulus_Operator() {
+    var modulus_math = 25 % 6;
+    document.getElementById("math_4").innerHTML = "When you divide 25 by 6 you have a remainder of " + modulus_math;
+}
+
+window.alert(Math.random());
+
+var X = 18;
+X++;
+var Y = X;
+document.write(Y).innerHTML;
+
+
+
